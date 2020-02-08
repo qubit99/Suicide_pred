b1c9b1e6c29b2ad5e0e615e539dbdf63239ab238
qubit99 <architranjan9@gmail.com>
2020-02-08 21:11:09 +0530

    Added redditdata

--- dataset/praw/redditdata.py
+++ dataset/praw/redditdata.py
@@ -1,34 +1,34 @@
 import praw
 import time
 import csv
+import json
 
 search_queries = {'kill me', 'suicide', 'suicidal', 'my friend is suicidal', 'feel like dying',
                   'depressed', 'killing myself', 'self harm', 'ready to end my life', 'raped', 'live anymore', 'my child is having suicidal intent', 'son is suicidal',
-                  'daughter is suicidal', 'ignores my problems', 'fucking die', 'lost my job', 'no reason to live', 'cut my body', 'kill myself', 'cheated on me'}
-
-
-
+                  'daughter is suicidal', 'ignores my problems', 'fucking die', 'lost my job', 'no reason to live', 'cut my body', 'kill myself', 'cheated on me', 'i cry', 'life sucks'}
 
 
 if __name__ == "__main__":
 
-
     reddit = praw.Reddit(client_id='jazU2BeYT1YamQ', client_secret="tMctKBrH3DunCFkqpOR15uVP5IM",
-                        password='anshul2007', user_agent='personal script by /u/qubit',
-                        username='architranjan9')
+                         password='anshul2007', user_agent='personal script by /u/qubit',
+                         username='architranjan9')
 
     print(reddit.user.me())
 
-    posts = [["head","post"]]
+    data = {}
+    data['posts'] = []
 
     for string in search_queries:
         time.sleep(1)
-        for post in reddit.subreddit('all').search(string,sort = "relevance"):
-            title = post.title.encode('utf-8')
-            post = post.selftext.encode('utf-8')
-            posts.append([title,post])
-    
-    with open('redditdata.csv', 'w', newline='') as file:
-        writer = csv.writer(file)
-        writer.writerows(posts)
-            
+        for post in reddit.subreddit('all').search(string, sort="relevance"):
+            title = post.title
+            postbody = post.selftext
+            data['posts'].append({
+                'post': title+postbody,
+                'intent': ''
+            })
+            print(title)
+
+    with open('redditdata.txt', 'w', newline='') as file:
+        json.dump(data, file)
